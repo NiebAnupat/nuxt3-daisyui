a1a939f952bc22247c75c314cd44671b77b8836f
NiebAnupat <anupat.k64@rsu.ac.th>
2023-12-14 02:54:48 +0700

    init nuxt3 with daisyui project

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <html data-theme="cmyk">
+    <slot />
+  </html>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>สวัสดี</div>
+  <div class="btn btn-primary ">ปุ่มจ้า</div>
+</template>
